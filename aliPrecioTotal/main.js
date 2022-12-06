b84f86f3314dd10277d7af0937a4d6caeecada95
laresistenciadelbit <noelperal@yahoo.es>
2022-12-06 22:30:34 +0100

    -Se adapta el código para aceptar peticiones devueltas de servidores cacheados con otra estructura de la web (tanto al buscar la clase como al manejarla) -Si no existe la clase por defecto, ni hay productos con precio de envío cargados, obtiene la clase a partir de los productos con envío gratis -arreglo al verificar que realmente no ha encontrado ninguna clase

--- aliPrecioTotal/main.js
+++ aliPrecioTotal/main.js
@@ -1,48 +1,67 @@
-let searchingClass="vcAi3";
 let doThis=true;
 if(!window.location.pathname.includes("/wholesale"))
 	doThis=false;
 
-//Cuando no utiliza la clase "vcAi3" la buscamos aquí a partir de la string "+envío:"
+let searchingClass="vcAi3";
+if(document.getElementsByClassName(searchingClass).length == 0)
+	searchingClass="v3--servicesText--1DI-jq_";
+
+//Cuando no utiliza la clase vcAi3 ni la de servicesText, la buscamos aquí a partir de la string envío
 if ( doThis && document.getElementsByClassName(searchingClass).length == 0 ) {
 	let limit=50;
-	let shippingCostIndex = document.body.innerHTML.indexOf("+envío:");
+	let shippingCostIndex = document.body.innerHTML.indexOf("+envío"+":");
 	
-	//if comentado; servía para que me funcione el código con el addon "javascript" de "chee"
-	//if(document.body.innerHTML.indexOf("let searchingClass=") > -1 && shippingCostIndex > -1 && shippingCostIndex > document.body.innerHTML.indexOf("let searchingClass="))
-	//	shippingCostIndex = -1;
+	if(shippingCostIndex < 0) {
+		shippingCostIndex = document.body.innerHTML.indexOf("+ envío"+":"); //si viene de un servidor cacheado viene así
+	}
+	
+	//puede no haber cargado aún ningún producto con gastos de envío, pero esta string aparece más veces
+	if(shippingCostIndex < 0) {
+		let indexAux = document.body.innerHTML.indexOf("Envío "+"gratis");
+		shippingCostIndex = indexAux+100+document.body.innerHTML.substr(indexAux+100).indexOf("Envío "+"gratis");
+	}
 	
 	if(shippingCostIndex > -1) {
 		let classStartIndex = shippingCostIndex - limit + (document.body.innerHTML.substr(shippingCostIndex-limit, limit)).lastIndexOf("=")+2;
 		searchingClass = document.body.innerHTML.substr(classStartIndex, shippingCostIndex-2-classStartIndex);
 	} else {
-		searchingClass = -1;
+		searchingClass = null;
 	}
 }
 
 function patata() {
+	let auxobj;
 	document.querySelectorAll("."+searchingClass).forEach(function(shippingCostSpan) {
-		if(shippingCostSpan.textContent[0]=='+' && (shippingCostSpan.textContent).indexOf("€")>-1)
+		// este selector será diferente en base a si viene de servidor cacheado o no
+		if(typeof shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[0] !== 'undefined') {
+			auxobj=shippingCostSpan.parentElement.parentElement.firstChild.firstChild;
+		} else if(typeof shippingCostSpan.parentElement.parentElement.parentElement.children[1].children[0] !== 'undefined') {
+			auxobj=shippingCostSpan.parentElement.parentElement.parentElement.children[1];
+		} else {
+			auxobj=null
+		}
+
+		if(auxobj && shippingCostSpan.textContent[0]=='+' && (shippingCostSpan.textContent).indexOf("€")>-1)
 		{
 			let price = parseFloat(
-				shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[0].textContent + 
+				auxobj.children[0].textContent + 
 				"." +
-				shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[2].textContent
+				auxobj.children[2].textContent
 			);
 
 			let shippingCost=parseFloat(shippingCostSpan.textContent.substr(shippingCostSpan.textContent.lastIndexOf("€")+2).replace(",","."));
 
 			for(let i=0;i<4;i++)
 			{
-				shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[i].style.color="grey";
-				shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[i].style.fontSize="xx-small";
+				auxobj.children[i].style.color="grey";
+				auxobj.children[i].style.fontSize="xx-small";
 			}
 			let newSpan=document.createElement("span");
 			newSpan.append( (price+shippingCost).toFixed(2) + "€" );
 			newSpan.style.fontSize="medium";
 
-			shippingCostSpan.parentElement.parentElement.firstChild.firstChild.children[3].textContent += "    +envío = ";
-			shippingCostSpan.parentElement.parentElement.firstChild.firstChild.append(newSpan);
+			auxobj.children[3].textContent += "    +envío = ";
+			auxobj.append(newSpan);
 			shippingCostSpan.textContent=shippingCostSpan.textContent.substr(1);
 		}
 		shippingCostSpan.classList.remove(searchingClass);
