1d533d85a4a2e13e366a15e03918b834f3d1cdc4
laresistenciadelbit <noelperal@yahoo.es>
2022-12-08 16:26:21 +0100

    -Ahora se limpia y recalcula continuamente la suma de precio+envío dado que al paginar los productos el malvado ajax de aliexpress carga encima de los mismos bloques el nuevo contenido sin reparar en si tienen contenido extra y sin limpiarlo. -Arreglos en precios con millares -Arreglos si en la nueva petición el envío de un producto es gratis, no se limpia el estilo gris aplicado en el producto previo al ajax

--- aliPrecioTotal/main.js
+++ aliPrecioTotal/main.js
@@ -2,6 +2,7 @@ let doThis=true;
 if(!window.location.pathname.includes("/wholesale"))
 	doThis=false;
 
+let plusSendString="+envío =";
 let searchingClass="vcAi3";
 if(document.getElementsByClassName(searchingClass).length == 0)
 	searchingClass="v3--servicesText--1DI-jq_";
@@ -10,17 +11,17 @@ if(document.getElementsByClassName(searchingClass).length == 0)
 if ( doThis && document.getElementsByClassName(searchingClass).length == 0 ) {
 	let limit=50;
 	let shippingCostIndex = document.body.innerHTML.indexOf("+envío"+":");
-	
+
 	if(shippingCostIndex < 0) {
 		shippingCostIndex = document.body.innerHTML.indexOf("+ envío"+":"); //si viene de un servidor cacheado viene así
 	}
-	
+
 	//puede no haber cargado aún ningún producto con gastos de envío, pero esta string aparece más veces
 	if(shippingCostIndex < 0) {
 		let indexAux = document.body.innerHTML.indexOf("Envío "+"gratis");
 		shippingCostIndex = indexAux+100+document.body.innerHTML.substr(indexAux+100).indexOf("Envío "+"gratis");
 	}
-	
+
 	if(shippingCostIndex > -1) {
 		let classStartIndex = shippingCostIndex - limit + (document.body.innerHTML.substr(shippingCostIndex-limit, limit)).lastIndexOf("=")+2;
 		searchingClass = document.body.innerHTML.substr(classStartIndex, shippingCostIndex-2-classStartIndex);
@@ -41,39 +42,70 @@ function patata() {
 			auxobj=null
 		}
 
-		if(auxobj && shippingCostSpan.textContent[0]=='+' && (shippingCostSpan.textContent).indexOf("€")>-1)
-		{
-			let price = parseFloat(
-				auxobj.children[0].textContent + 
-				"." +
-				auxobj.children[2].textContent
-			);
-
-			let shippingCost=parseFloat(shippingCostSpan.textContent.substr(shippingCostSpan.textContent.lastIndexOf("€")+2).replace(",","."));
-
-			for(let i=0;i<4;i++)
-			{
-				auxobj.children[i].style.color="grey";
-				auxobj.children[i].style.fontSize="xx-small";
+		if(auxobj && (auxobj.lastChild.textContent).indexOf("€") == (auxobj.lastChild.textContent).length-1 ) {
+		//si está recalculando hay que eliminar lo existente! (ya que al paginar carga por ajax el contenido sobre los mismos cmapos, así que me obliga a recalcular los ya calculados :S)
+			//si el children 1 o 3 tiene plusSendString , de ese span hay que eliminar todo (igualarlo a "€")
+			let repeated = 0;
+			if(auxobj.children[1].textContent.indexOf(plusSendString) > -1) {
+				repeated = 1;
+			} else {
+				if(typeof auxobj.children[3] !== 'undefined' && auxobj.children[3].textContent.indexOf(plusSendString) > -1) { //si hay decimales
+					repeated = 3;
+				} else {
+					if(typeof auxobj.children[5] !== 'undefined' && auxobj.children[5].textContent.indexOf(plusSendString) > -1) { //si hay miles y decimales
+					repeated = 5;
+					}
+				}
 			}
-			let newSpan=document.createElement("span");
-			newSpan.append( (price+shippingCost).toFixed(2) + "€" );
-			newSpan.style.fontSize="medium";
+			if(repeated)
+			{
+				auxobj.children[repeated].textContent="€";
+				//si eso fue así, además hay que eliminar el último hijo de de auxobj (el total que se ha calculado)
+				auxobj.lastChild.remove();
+			}
+			if(
+				( shippingCostSpan.textContent[0]=='·' || shippingCostSpan.textContent[0]=='+' ) &&
+				(shippingCostSpan.textContent).indexOf("€") > -1
+			) {
+				let price =	parseFloat(auxobj.children[0].textContent);
+				if(auxobj.children[1].textContent == ',') {
+					price += parseFloat("0."+auxobj.children[2].textContent);
+				} else {
+					if(auxobj.children[1].textContent == '.') //para millares paso de sumarle los decimales, porque habría que comprobar también si los tiene o no...
+						price = parseFloat(auxobj.children[0].textContent + auxobj.children[2].textContent);
+				}
 
-			auxobj.children[3].textContent += "    +envío = ";
-			auxobj.append(newSpan);
-			shippingCostSpan.textContent=shippingCostSpan.textContent.substr(1);
+				let shippingCost=parseFloat(shippingCostSpan.textContent.substr(shippingCostSpan.textContent.lastIndexOf("€")+2).replace(",","."));
+				for(let i=0;i<auxobj.children.length;i++) {
+					auxobj.children[i].style.color="grey";
+					auxobj.children[i].style.fontSize="xx-small";
+				}
+				let newSpan=document.createElement("span");
+				newSpan.append( (price+shippingCost).toFixed(2) + "€" );
+				newSpan.style.fontSize="medium";
+
+				auxobj.lastChild.textContent += " "+plusSendString+" ";
+				auxobj.append(newSpan);
+				shippingCostSpan.textContent="·"+shippingCostSpan.textContent.substr(1); //cambiamos el + por ·
+				shippingCostSpan.style.color="grey";
+				shippingCostSpan.style.fontSize="xx-small";
+			} else {
+				if((shippingCostSpan.textContent).indexOf("gratis") > -1) {
+					for(let i=0;i<auxobj.children.length;i++) {
+						auxobj.children[i].style.color="black";
+						auxobj.children[i].style.fontSize="medium";
+					}
+				}
+			}
 		}
-		shippingCostSpan.classList.remove(searchingClass);
 	});
 }
 
 function lookForChanges() {
-	if ( document.getElementsByClassName(searchingClass).length > 0 )
-		patata();
+	patata();
 }
 
 if(doThis && searchingClass) {
 	patata();
-	setInterval(lookForChanges, 5000);
+	setInterval(lookForChanges, 4500);
 }
